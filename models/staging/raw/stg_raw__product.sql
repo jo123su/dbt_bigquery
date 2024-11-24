a661564073bf6dd057df085141a536de66fce646
josuecontrerasulca <josue.contreras.sulca@gmail.com>
2024-11-24 00:17:02 +0000

    Update 19:16

--- models/staging/raw/stg_raw__product.sql
+++ models/staging/raw/stg_raw__product.sql
@@ -10,8 +10,7 @@ renamed as (
 
     select
         products_id,
-        purchse_price
-
+        CAST(purchse_price AS FLOAT64) AS purchase_price
     from source
 
 )
--- models/staging/raw/stg_raw__ship.sql
+++ models/staging/raw/stg_raw__ship.sql
@@ -1,15 +1,17 @@
 with
-    source as (select * from {{ source("raw", "ship") }}),
+    source as (
+        select * 
+        from {{ source("raw", "ship") }}
+    ),
 
     renamed as (
-
         select 
-        orders_id,
-        shipping_fee,
-        logcost,
-        ship_cost
+            orders_id,
+            CAST(ship_cost AS INT) AS ship_cost, -- Casteo corregido
+            shipping_fee,
+            logCost
         from source
-
     )
+
 select *
 from renamed
